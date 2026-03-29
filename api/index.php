c895a90b4810ce78e04f8e86d83f6dc299b0644f
SidimaD <Sidihassan17@gmail.com>
2026-03-29 21:52:32 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,19 +1,24 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 
-// We use a simple curl to avoid 'file_get_contents' restriction on some runtimes
-$ch = curl_init("https://link.theplatform.eu/s/dmimain/media/dmi-prod-live-media-dubaisports1?format=SMIL&formats=MPEG-DASH");
+$channel = "dubaisports1";
+$api = "https://link.theplatform.eu/s/dmimain/media/dmi-prod-live-media-" . $channel . "?format=SMIL&formats=MPEG-DASH";
+
+// Use CURL - it's more stable on Vercel
+$ch = curl_init($api);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/149.0.0.0 Safari/537.36');
 $res = curl_exec($ch);
 curl_close($ch);
 
-preg_match('/src="([^"]+)"/', $res, $m);
-
-if (isset($m[1])) {
+// Find the Akamai URL with the hdntl token
+if (preg_match('/src="([^"]+)"/', $res, $m)) {
     $link = str_replace('&amp;', '&', $m[1]);
     header("Location: " . $link);
     exit;
 } else {
-    echo "Check failed: SMIL Source Unreachable";
+    header("HTTP/1.1 500 Internal Server Error");
+    echo "Error: Akamai token could not be generated.";
 }
