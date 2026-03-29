a38ab7b1142dbb316b788e24052c740d0c3cc73e
SidimaD <Sidihassan17@gmail.com>
2026-03-29 21:55:34 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,24 +1,18 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 
-$channel = "dubaisports1";
-$api = "https://link.theplatform.eu/s/dmimain/media/dmi-prod-live-media-" . $channel . "?format=SMIL&formats=MPEG-DASH";
-
-// Use CURL - it's more stable on Vercel
-$ch = curl_init($api);
+// Dubai Sports 1 Handshake
+$ch = curl_init("https://link.theplatform.eu/s/dmimain/media/dmi-prod-live-media-dubaisports1?format=SMIL&formats=MPEG-DASH");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/149.0.0.0 Safari/537.36');
 $res = curl_exec($ch);
 curl_close($ch);
 
-// Find the Akamai URL with the hdntl token
 if (preg_match('/src="([^"]+)"/', $res, $m)) {
     $link = str_replace('&amp;', '&', $m[1]);
     header("Location: " . $link);
     exit;
 } else {
-    header("HTTP/1.1 500 Internal Server Error");
-    echo "Error: Akamai token could not be generated.";
+    echo "ERROR: Akamai Token not found.";
 }
